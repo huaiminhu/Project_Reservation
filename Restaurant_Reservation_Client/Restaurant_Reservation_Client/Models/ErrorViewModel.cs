487cbf5f5ceef849cb4947c9de0aee89c2431760
Hank Hu <hankhuwm@gmail.com>
2024-11-07 22:36:12 +0800

    Project Reservation 1st commit

---
+++ Restaurant_Reservation_Client/Restaurant_Reservation_Client/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Restaurant_Reservation_Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
