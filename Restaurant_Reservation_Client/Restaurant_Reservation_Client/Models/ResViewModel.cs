85e851e36abb2dab088ab8a13b8ba3f7503886e9
Hank Hu <hankhuwm@gmail.com>
2024-11-30 15:40:47 +0800

    113/11/30 測試更新

--- Restaurant_Reservation_Client/Restaurant_Reservation_Client/Models/ResViewModel.cs
+++ Restaurant_Reservation_Client/Restaurant_Reservation_Client/Models/ResViewModel.cs
@@ -5,6 +5,6 @@ namespace Restaurant_Reservation_Client.Models
     public class ResViewModel
     {
         public List<ArrivedTime> Periods { get; set; }
-        public List<Reservation> Reservations { get; set; }
+        public Reservation Reservation { get; set; }
     }
 }
