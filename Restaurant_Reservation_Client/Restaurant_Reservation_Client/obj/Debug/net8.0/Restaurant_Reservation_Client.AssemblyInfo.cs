b311cc46cfe38bf424bc2593e2a336549f485535
Hank Hu <hankhuwm@gmail.com>
2024-12-24 21:29:56 +0800

    Client端更新: 首頁顯示開放時段及座位數顯示顏色及字體大小

--- Restaurant_Reservation_Client/Restaurant_Reservation_Client/obj/Debug/net8.0/Restaurant_Reservation_Client.AssemblyInfo.cs
+++ Restaurant_Reservation_Client/Restaurant_Reservation_Client/obj/Debug/net8.0/Restaurant_Reservation_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant_Reservation_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24e5aa7ff572ce466e665ba488a19e93a9f51e5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43b1b2ff6883695da844d42d2a79925d5374eded")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant_Reservation_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant_Reservation_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
