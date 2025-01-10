629c597257bb1a44bc66799fc5c49d50731fb5c0
Hank Hu <hankhuwm@gmail.com>
2025-01-10 22:06:16 +0800

    上傳圖片1

--- Restaurant_Reservation_Client/Restaurant_Reservation_Client/obj/Debug/net8.0/Restaurant_Reservation_Client.AssemblyInfo.cs
+++ Restaurant_Reservation_Client/Restaurant_Reservation_Client/obj/Debug/net8.0/Restaurant_Reservation_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant_Reservation_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c210adb0484f76fd48dcfa523bd01c8b24000d36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5841be52daef165b174e5e94732f4828e4877253")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant_Reservation_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant_Reservation_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
