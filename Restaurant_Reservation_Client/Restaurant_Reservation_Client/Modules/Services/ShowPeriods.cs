ea347f2044c3319cd1e46b78753db560db690c22
Hank Hu <hankhuwm@gmail.com>
2025-01-10 22:43:23 +0800

    更改時段已過限制: 差距從1改為0

--- Restaurant_Reservation_Client/Restaurant_Reservation_Client/Modules/Services/ShowPeriods.cs
+++ Restaurant_Reservation_Client/Restaurant_Reservation_Client/Modules/Services/ShowPeriods.cs
@@ -22,7 +22,7 @@ namespace Restaurant_Reservation_Client.Modules.Services
                 {
                     int remainSeat = 45 - reservations.Where(r => r.arrivalTimeId == arrivalTimes[i].Id).Sum(s => s.SeatRequirement);
                     var diff = Convert.ToInt32((DateTime.Parse(arrivalTimes[i].Period[0..5]) - DateTime.Now).TotalHours);
-                    if (remainSeat > 0 && diff > 1)
+                    if (remainSeat > 0 && diff > 0)
                         results.Add(new DisplayViewModel { Id = arrivalTimes[i].Id, Display = arrivalTimes[i].Period + string.Format("\t(目前剩餘空位:{0})", remainSeat) });
                 }
             }
