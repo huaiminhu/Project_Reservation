c283ace349b361234a0a4f4372c6475425dbebf0
Hank Hu <hankhuwm@gmail.com>
2025-01-20 22:23:28 +0800

    API SERVER新增模型: Member(會員), Activity(優惠)以及Record(訂位紀錄)

---
+++ Restaurant_Reservation_API_Server/Restaurant_Reservation_API_Server/Models/Record.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurant_Reservation_API_Server.Models
+{
+    public class Record
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        // 訂位日期
+        public DateTime BookingDate { get; set; }   
+
+        // 訂位時段
+        public int ArrivalTimeId { get; set; }   
+        public ArrivalTime? ArrivalTime { get; set; }
+
+        // 會員
+        public int MemberId { get; set; }   
+        public Member? Member { get; set; }
+    }
+}
